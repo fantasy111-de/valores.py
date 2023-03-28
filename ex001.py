5aaa3099c17137438fe4cd7c9ae0c008cb99fc0a
vivi <vicunico6866@gmail.com>
2023-03-27 21:33:52 -0300

    Create ex001.py

---
+++ ex001.py
@@ -0,0 +1,6 @@
+val_01= int(input("Informe o primeiro valor"))
+val_02= int(input("Informe o segundo valor"))
+if (val_01>val_02):
+    print("O valor maior é:", val_01,"!")
+else:
+    print("O valor maior é:", val_02,"!")
